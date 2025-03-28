129b241fa5e25150abd73d9a1656ef392c6440cd
Elson18 <elsonaron54@gmail.com>
2025-03-28 07:30:55 +0530

    Detecting Distance of the person

---
+++ Ultrasonic sensor.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+# Ultrasonic sensor setup
+TRIG = 23  # GPIO pin for Trigger
+ECHO = 24  # GPIO pin for Echo
+
+def setup_ultrasonic():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+    GPIO.output(TRIG, False)
+    time.sleep(2)
+
+def get_distance():
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+    
+    start_time = time.time()
+    stop_time = time.time()
+    
+    while GPIO.input(ECHO) == 0:
+        start_time = time.time()
+    
+    while GPIO.input(ECHO) == 1:
+        stop_time = time.time()
+    
+    elapsed_time = stop_time - start_time
+    distance = (elapsed_time * 34300) / 2  
+    return round(distance, 2)  
